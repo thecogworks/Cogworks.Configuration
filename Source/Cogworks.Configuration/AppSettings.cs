77aa71fb1b7642af44f580d64e6217c579b6207c
gmargol <greg.margol@wearecogworks.com>
2020-03-04 13:53:08 +0100

    initial commit

---
+++ Source/Cogworks.Configuration/AppSettings.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Configuration;
+using Umbraco.Core;
+
+namespace Cogworks.Configuration
+{
+    public static class AppSettings
+    {
+        public static T Get<T>(string key, bool tryConvertTo = false)
+        {
+            var value = default(T);
+            var setting = ConfigurationManager.AppSettings[key];
+
+            if (string.IsNullOrWhiteSpace(setting)) return value;
+
+            if (tryConvertTo)
+            {
+                var attemptConvert = setting.TryConvertTo<T>();
+                if (attemptConvert.Success)
+                {
+                    value = attemptConvert.Result;
+                }
+            }
+            else
+            {
+                var converter = TypeDescriptor.GetConverter(typeof(T));
+                value = (T)converter.ConvertFromInvariantString(setting);
+            }
+
+            return value;
+        }
+    }
+}
