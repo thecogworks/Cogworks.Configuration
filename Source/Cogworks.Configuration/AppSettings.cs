6322c1701c0d2a1112eb8b0211369ee6ba177227
Adrian Ochmann <adrian.ochmann@outlook.com>
2020-03-09 09:53:29 +0100

    feat: removed dependencies that are not netstandard

--- Source/Cogworks.Configuration/AppSettings.cs
+++ Source/Cogworks.Configuration/AppSettings.cs
@@ -1,31 +1,22 @@
 ﻿using System.ComponentModel;
 using System.Configuration;
-using Umbraco.Core;
 
 namespace Cogworks.Configuration
 {
     public static class AppSettings
     {
-        public static T Get<T>(string key, bool tryConvertTo = false)
+        public static T Get<T>(string key)
         {
             var value = default(T);
             var setting = ConfigurationManager.AppSettings[key];
 
-            if (string.IsNullOrWhiteSpace(setting)) return value;
+            if (string.IsNullOrWhiteSpace(setting))
+            {
+                return value;
+            }
 
-            if (tryConvertTo)
-            {
-                var attemptConvert = setting.TryConvertTo<T>();
-                if (attemptConvert.Success)
-                {
-                    value = attemptConvert.Result;
-                }
-            }
-            else
-            {
-                var converter = TypeDescriptor.GetConverter(typeof(T));
-                value = (T)converter.ConvertFromInvariantString(setting);
-            }
+            var converter = TypeDescriptor.GetConverter(typeof(T));
+            value = (T)converter.ConvertFromInvariantString(setting);
 
             return value;
         }
